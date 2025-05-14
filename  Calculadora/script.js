a31506b3ac6ecb109f50915bec78ed71b8f7b6d2
Gabriel Gasparotto Pantaleão <gabrielgasparotto45@gmail.com>
2025-05-14 14:04:15 +0000

    Adicionando arquivos de prompt para o projeto

---
+++  Calculadora/script.js
@@ -0,0 +1,11 @@
+import prompt from 'prompt-sync';
+// receber 
+function receberExpressao() {
+    let expr = prompt("Digite a sua f(x): ");
+    console.log(expr);
+    return expr;
+}
+
+function parseExpressao(expr) {
+    
+}
